6b154b6206569c25276412f25f03617269d552f8
Aaro <aaro.kivimaki@gmail.com>
2025-02-15 20:09:58 +0200

    got list adding working plus some prettier looking background

--- js/script.js
+++ js/script.js
@@ -1,4 +1,18 @@
-document.addEventListener("click", () => {
-  console.log("Works");
-  document.getElementById("taskList").innerHTML += "jee";
+const addTaskButton = document.getElementById("addButton")
+const textInput = document.getElementById("inputText")
+const addList = document.getElementById("taskList")
+
+addTaskButton.addEventListener("click", () => {
+  const newList = document.createElement("li")
+  const textInputValue = textInput.value
+  const createTextPlace = document.createTextNode(textInputValue)
+  newList.appendChild(createTextPlace)
+
+  if (textInputValue == "") {
+    alert("Can't add nothing")
+  } else {
+    addList.appendChild(newList)
+  }
+
+  textInput.value = ""
 });
