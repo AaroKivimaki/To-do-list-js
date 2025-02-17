16b79690794a66d096a5dc00ec638eb890e9b19e
AaroKivimäki <aaro.kivimaki@gmail.com>
2025-02-17 17:42:50 +0200

    Got the every other background color change working. Next trying to get close button

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
+const getListClasses = document.getElementsByTagName("lists");
+for (let i = 0; i < getListClasses.length; i++) {
+  const newSpan = document.createElement("span");
+  const spanX = document.createTextNode("X");
+  newSpan.classList.add("closeButton");
+  newSpan.appendChild(spanX);
+  getListClasses[i].appendChild(newSpan);
+}
+
 const addTaskButton = document.getElementById("addButton");
 const textInput = document.getElementById("inputText");
 const addList = document.getElementById("taskList");
@@ -7,6 +16,8 @@ addTaskButton.addEventListener("click", () => {
   const textInputValue = textInput.value;
   const createTextPlace = document.createTextNode(textInputValue);
   newList.appendChild(createTextPlace);
+
+  newList.classList.add("lists");
 
   if (textInputValue == "") {
     alert("Can't add nothing");
